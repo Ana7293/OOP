b990243aa7d8f5ac5564d29843372d86f88078da
ana7293 <ana7293nik@gmail.com>
2023-03-03 01:47:08 +0500

    Lesson2/21

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        Human person = new Human("Иван", 56);
-        person.setAge(-40);
-        System.out.println("Имя - " + person.getName()  + ". Возраст - " + person.getAge());
+        System.out.println(MyMath.length(10));
+        System.out.println(MyMath.area(10));
+        System.out.println(MyMath.sum(4,5,6,7,8,0,5,4,3,9));
 
     }
 }
--- src/MyMath.java
+++ src/MyMath.java
@@ -1,12 +1,28 @@
 public class MyMath {
-   static int multiply (int a, int b){
+
+    public static final double PI = 3.14;
+
+    public static double length(double radius) {
+    return 2 * PI * radius;
+}
+    public static double area(double radius) {
+    return  PI * radius * radius;
+    }
+    public static int sum(int... numbers) {         //метод возвращает сумму всех аргументов (... )
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+    public static int multiply (int a, int b){
         return a * b;
     }
 
-   static double multiply (double a, double b){               //перегрузка метода
+    public static double multiply (double a, double b){               //перегрузка метода
         return a * b;
     }
-    static int multiply (int a){
+    public static  int multiply (int a){
         return a * a;
     }
 }
